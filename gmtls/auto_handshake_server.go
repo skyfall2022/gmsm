99d29f5cac93d95bed75f77f38bfddc2b75dd1d2
skyfall2022 <115613048+skyfall2022@users.noreply.github.com>
2022-10-14 15:58:20 +0800

    Update auto_handshake_server.go

--- gmtls/auto_handshake_server.go
+++ gmtls/auto_handshake_server.go
@@ -8,6 +8,7 @@ import (
 	"crypto/rsa"
 	"errors"
 	"fmt"
+	"log"
 	"io"
 	"sync/atomic"
 	"time"
@@ -48,7 +49,7 @@ func (c *Conn) serverHandshakeAutoSwitch() error {
 			return err
 		}
 		// 运行 GMSSL 握手流程
-		fmt.Printf("src ip:%+v, recv gm-tls handshake, ver:%+v", c.RemoteAddr(), c.vers)
+		log.Printf("src ip:%+v, recv gm-tls handshake, ver:%+v", c.RemoteAddr(), c.vers)
 		return runServerHandshakeGM(c, hs, isResume)
 	case VersionSSL30, VersionTLS10, VersionTLS11, VersionTLS12:
 		// SSL v3.0 - TLS 1.3
@@ -63,7 +64,7 @@ func (c *Conn) serverHandshakeAutoSwitch() error {
 			return err
 		}
 		// 运行 TLS 握手流程
-		fmt.Printf("src ip:%+v, recv tls handshake, ver:%+v", c.RemoteAddr(), c.vers)
+		log.Printf("src ip:%+v, recv tls handshake, ver:%+v", c.RemoteAddr(), c.vers)
 		return runServerHandshake(c, hs, isResume)
 	default:
 		_ = c.sendAlert(alertProtocolVersion)
