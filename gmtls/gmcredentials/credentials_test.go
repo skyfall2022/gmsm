a512b06be75561d53b7960ba535c50ba192d983b
skyfall2022 <115613048+skyfall2022@users.noreply.github.com>
2022-10-17 16:28:44 +0800

    Update credentials_test.go

--- gmtls/gmcredentials/credentials_test.go
+++ gmtls/gmcredentials/credentials_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/tjfoc/gmsm/gmtls"
-	"github.com/tjfoc/gmsm/gmtls/gmcredentials/echo"
-	"github.com/tjfoc/gmsm/x509"
+	"github.com/skyfall2022/gmsm/gmtls"
+	"github.com/skyfall2022/gmsm/gmtls/gmcredentials/echo"
+	"github.com/skyfall2022/gmsm/x509"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
